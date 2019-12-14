6b288adf0b57f4a0b08ea0207d40bc385df86a32
Nicola Landro <nicolaxx94@live.it>
2019-12-14 12:25:24 +0100

    pypi lib

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ setup(
     description="A small pyqt5 gui to use midi knobs for change linux master audio",
     version="0.0.1",
     python_requires='>=3.5',
-    install_requires=["python-rtmidi", "mido", "vext.pyqt5"],
+    install_requires=["python-rtmidi", "mido", "vext.pyqt5", "PyQt5"],
     packages=find_packages(),
     entry_points={
-        'console_scripts': ['midiknobs=midiknobs.__main__:main'],
+        'console_scripts': ['midiknobs_gui=midiknobs.__main__:main'],
     },
 )
